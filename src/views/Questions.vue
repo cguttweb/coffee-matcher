87ec72dbe56d5319b8e7658f7b3197c0ea1c6bff
Chloe <cguttweb@pm.me>
2019-07-02 19:16:33 +0100

    updated jquery

--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -2,17 +2,22 @@
   <div>
     <b-container>
       <b-form-input
-        v-model="text"
+        v-model="name"
         label="Enter your name"
         placeholder="Enter your name"
       ></b-form-input>
       <h3 class="mt-2">Hello {{ text }}</h3>
       <h4>Answer the following questions to be matched to a coffee</h4>
       <label for="milkoption">Do you like milk in your coffee?</label>
-      <b-form-select name="milkoption" v-model="selected" class="mb-3">
+      <b-form-select name="milkoption" v-model="milkoption" class="mb-3">
         <option :value="null">Please select an option</option>
         <option value="yes">Yes</option>
         <option value="no">No</option>
+      </b-form-select>
+      <b-form-select>
+        <option value=""></option>
+        <option value=""></option>
+        <option value=""></option>
       </b-form-select>
     </b-container>
   </div>
@@ -22,7 +27,7 @@
 export default {
   data() {
     return {
-      text: ""
+      name: ""
     };
   }
 };
