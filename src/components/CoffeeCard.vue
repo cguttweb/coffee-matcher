5ca22deeb4bb338dc1ceebf28e5472858af99025
Chloe <cguttweb@pm.me>
2019-07-18 20:40:37 +0100

    added component

---
+++ src/components/CoffeeCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="card" style="width: 18rem;">
+      <img
+        src="http://via.placeholder.com/200x150"
+        class="card-img-top"
+        alt="photo of coffee"
+      />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CoffeeCard"
+};
+</script>
+
+<style></style>
