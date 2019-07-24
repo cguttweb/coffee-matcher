4ec26a0c3881f61837ae8844e8f2063e8b98d626
Chloe <cguttridge@clever-touch.com>
2019-07-24 09:01:45 +0100

    added coffeesjs to card component

--- src/components/CoffeeCard.vue
+++ src/components/CoffeeCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <div class="card" style="width: 18rem;">
-      <img :src="photo" class="card-img-top" alt="photo of coffee" />
+      <img :src=photo class="card-img-top" alt="photo of coffee" />
       <div class="card-body">
         <h5 class="card-title" :name="coffeename"></h5>
-        <p class="card-text" :description="description"></p>
+        <p class="card-text" :description=description></p>
         <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
       </div>
     </div>
@@ -16,15 +16,93 @@ import '../coffees.js';
 
 export default {
   name: 'CoffeeCard',
+  data(){
+    return {
+      coffees: [
+      {
+        espresso: {
+          name: 'espresso',
+          description: '',
+          photo: ''
+        },
+        ristresso: {
+        name: "",
+        description: "",
+        photo: ""
+        },
+        latte: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        cappuccino: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        mocha: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        cortado: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        americano: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        macchiato: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        espresso con panna: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        piccolo: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        flat white: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        lungo: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        affogato: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        espresso martini: {
+          name: "",
+          description: "",
+          photo: ""
+        },
+        cold brew: {
+          name: "",
+          description: "",
+          photo: ""
+        }
+      }
+    ]
+    }
+  },
   props: {
     name: String,
     description: String,
     photo: String
-  },
-  data() {
-    return {
-      coffeename: this.name
-    };
   }
 };
 </script>
