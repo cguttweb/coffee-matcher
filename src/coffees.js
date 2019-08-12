c18bfe879867ad65d4439bca6630996e7364ccd9
Chloe <cguttweb@pm.me>
2019-08-12 21:32:31 +0100

    added coffees info

--- src/coffees.js
+++ src/coffees.js
@@ -6,44 +6,44 @@ export default [
       "https://www.lavazza.co.uk/content/dam/lavazza/magazine/coffee-culture/the-coffee-book/espresso/lavazza-coffeebook-ecomeespresso-imgdx-d.png.transform/null/19df145319e179900947f02e36f8874cc3ba2a8e/img.png"
   },
   {
-    name: "ristresso",
+    name: "ristretto",
     description:
-      "stronger than an espresso seem as the perfect espresso by some",
+      "Ristretto means restricted or concentrated in Italian so this drink is a short shot of espresso made with half the amount of water compared to an espresso. It is seen as the perfect espresso by some.",
     photo: ""
   },
   {
     name: "latte",
-    description: "",
+    description: "A drink made with espresso and steamed milk. Can be made with different types of milk e.g. soya, almond or coconut.",
     photo: ""
   },
   {
     name: "cappuccino",
-    description: "",
+    description: "An espresso based drink with steamed milk foam it originates from Italy. The name is thought to come from the Capuchin friars referring to the colour of their habitats. Also available as an iced option.",
     photo: ""
   },
   {
     name: "mocha",
-    description: "",
+    description: "made with espresso and milk like a latte but with added chocolate flavouring usually cocoa powder. Often available as an iced option and in some cases there is a sweeter white chocolate mocha available.",
     photo: ""
   },
   {
     name: "cortado",
-    description: "",
+    description: "espresso with equal amount of steamed warm milk reducing the acidity. It originates from Spain.",
     photo: ""
   },
   {
     name: "americano",
-    description: "",
+    description: "Drink prepared by diluting an espresso with hot water resulting in a different flavour to traditionally brewed coffee.",
     photo: ""
   },
   {
     name: "macchiato",
-    description: "",
+    description: "In Italian macchiato means stained or spotted referring to the small amount of frothed milk used. Espresso coffee topped with a small amount of frothed milk.",
     photo: ""
   },
   {
     name: "espresso con panna",
-    description: "",
+    description: "means espresso with cream in Italian a single or double espresso topped with whipped cream",
     photo: ""
   },
   {
@@ -65,7 +65,7 @@ export default [
   },
   {
     name: "affogato",
-    description: "a shot of espresso poured over ice cream",
+    description: "Italian for drowned this is a coffee-based dessert with vanilla ice-cream topped or drowned with a shot of hot espresso",
     photo: ""
   },
   {
